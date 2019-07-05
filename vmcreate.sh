216aeaf8feccd2afff4c533a6c34d5d4c049f6ae
Ping Zhang <pizhang@redhat.com>
2019-07-05 14:55:39 +0800

    fixed the syntax error of sleep command

--- vmcreate.sh
+++ vmcreate.sh
@@ -491,7 +491,7 @@ if [ $STOP == "NO" ]; then
 	fi
 
     OVS_PATH=/mnt/share/vms/OVS
-	Sleep 30
+	/usr/bin/sleep 30
 	if [ $SAVED = "YES" ]; then
 		qemu-img convert -O qcow2 $image_path/$master_image ${IMAGE_NAME}.qcow2
 		#clean the vm and all spend storage
